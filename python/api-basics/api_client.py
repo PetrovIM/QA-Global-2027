df6ba23cce9add3672fc3c211369b1d6e28353b3
Ilya Petrv <petrov_im@rambler.ru>
2026-08-13 16:49:00 +0300

    Add two func: _build_url and _request

--- python/api-basics/api_client.py
+++ python/api-basics/api_client.py
@@ -1,25 +1,24 @@
 import requests
 
 class APIClient:
-    def __init__(self, base_url):
+    def __init__(self, base_url, headers=None):
         self.base_url = base_url
+        self.headers = headers or {}
+
+    def _build_url(self, endpoint):
+        return f"{self.base_url.rstrip("/")}/{endpoint.lstrip("/")}"
+
+    def _request(self, method, endpoint, data=None):
+        return requests.request(method, self._build_url(endpoint), json=data, headers=self.headers)
 
     def get(self, endpoint):
-        full_url = f"{self.base_url.rstrip("/")}/{endpoint.lstrip("/")}"
-        response = requests.get(full_url)
-        return response
+        return self._request("GET", endpoint)
 
     def post(self, endpoint, data):
-        full_url = f"{self.base_url.rstrip("/")}/{endpoint.lstrip("/")}"
-        response = requests.post(full_url, json=data)
-        return response
+        return self._request("POST", endpoint, data)
 
     def put(self, endpoint, data):
-        full_url = f"{self.base_url.rstrip("/")}/{endpoint.lstrip("/")}"
-        response = requests.put(full_url, json=data)
-        return response
+        return self._request("PUT", endpoint, data)
 
     def patch(self, endpoint, data):
-        full_url = f"{self.base_url.rstrip("/")}/{endpoint.lstrip("/")}"
-        response = requests.patch(full_url, json=data)
-        return response
+        return self._request("PATCH", endpoint, data)
---
+++ python/api-basics/test_data.py
@@ -0,0 +1,5 @@
+CREATE_USER_PAYLOAD = {
+    "name": "Ilya Petrov",
+    "username": "PetrovIM",
+    "email": "ilya@example.com"
+}
