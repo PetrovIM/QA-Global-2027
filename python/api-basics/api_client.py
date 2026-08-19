9ffcced938585915a96548b644929836f7e085a3
Ilya Petrv <petrov_im@rambler.ru>
2026-08-19 12:56:16 +0300

    Add tests

--- python/api-basics/api_client.py
+++ python/api-basics/api_client.py
@@ -1,18 +1,33 @@
 import requests
+from requests import session
+
 
 class APIClient:
     def __init__(self, base_url, headers=None):
         self.base_url = base_url
         self.headers = headers or {}
+        self.session = requests.Session()
 
     def _build_url(self, endpoint):
         return f"{self.base_url.rstrip("/")}/{endpoint.lstrip("/")}"
 
-    def _request(self, method, endpoint, data=None):
-        return requests.request(method, self._build_url(endpoint), json=data, headers=self.headers)
+    def _request(self, method, endpoint, data=None, params=None):
+        try:
+            response =  self.session.request(
+                method,
+                self._build_url(endpoint),
+                json=data,
+                headers=self.headers,
+                params=params,
+                timeout=10
+            )
+            return response
+        except requests.exceptions.RequestException as e:
+            raise RuntimeError(f"API request failed: {e}") from e
 
-    def get(self, endpoint):
-        return self._request("GET", endpoint)
+
+    def get(self, endpoint, params=None):
+        return self._request("GET", endpoint, params=params)
 
     def post(self, endpoint, data):
         return self._request("POST", endpoint, data)
