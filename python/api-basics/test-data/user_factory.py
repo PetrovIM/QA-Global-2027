a21a9dfef23adeb243e5331b6f52444daf03563f
Ilya Petrv <petrov_im@rambler.ru>
2026-09-09 13:50:28 +0300

    Add class User and UserFactory

---
+++ python/api-basics/test-data/user_factory.py
@@ -0,0 +1,7 @@
+from user import User
+
+
+class UserFactory:
+
+    def create_user(self, first_name, last_name, phone, email, address, status = 'new', gender = None):
+        return User(first_name=first_name, last_name=last_name, phone=phone, email=email, address=address, status=status, gender=gender)
